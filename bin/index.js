8498e55f62cbec1f7e4d868668a43fbf6fc5e847
Brandon Semilla <semibran@gmail.com>
2020-10-28 21:37:46 -0700

    add focus+parent model as more op order prep

--- bin/index.js
+++ bin/index.js
@@ -3,9 +3,12 @@ const strify = require("../lib/strify")
 const fnify = require("../lib/fn")
 
 let expr = process.argv[2]
+
 let token = parse(expr)
-let restr = strify(token)
-let fn = fnify(expr)
 console.log("token: " + JSON.stringify(token))
+
+let restr = strify(token)
 console.log("restr: " + restr)
+
+let fn = fnify(expr)
 console.log("fn: " + fn)
