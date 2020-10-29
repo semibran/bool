03eb679abdd58641a3f4337bfd73db6631798fd8
Brandon Semilla <semibran@gmail.com>
2020-10-28 20:54:17 -0700

    restr test

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,8 @@
 const parse = require("../lib/parse")
+const strify = require("../lib/strify")
 
-let input = process.argv[2]
-let output = parse(input)
-console.log(JSON.stringify(output))
+let expr = process.argv[2]
+let token = parse(expr)
+let restr = strify(token)
+console.log("token: " + JSON.stringify(token))
+console.log("restr: " + restr)
