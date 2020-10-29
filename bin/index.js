1c01a4a371d40278e73922d9386ab8b7c5ba7a58
Brandon Semilla <semibran@gmail.com>
2020-10-29 12:30:43 -0700

    spice up cli a bit

--- bin/index.js
+++ bin/index.js
@@ -3,23 +3,39 @@ const strify = require("../lib/strify")
 const fnify = require("../lib/fn")
 const reduceargs = require("../lib/args")
 
-let expr = process.argv[2]
+let prog = process.argv[2]
+let expr = process.argv[3]
+if (prog && !expr) {
+	expr = prog
+	prog = null
+}
+
 if (!expr) {
 	console.log("Enter a valid boolean expression, eg. x+y")
 	process.exit()
 }
 
 let token = parse(expr)
-console.log("token: " + JSON.stringify(token))
-
-let restr = strify(token)
-console.log("restr: " + restr)
+let fn = fnify(token)
+if (prog === "parse") {
+	console.log(JSON.stringify(token) + "\n")
+	process.exit()
+} else if (prog === "fnify") {
+	console.log(fn + "\n")
+	process.exit()
+} else if (prog === "debug") {
+	let restr = strify(token)
+	console.log("token: " + JSON.stringify(token))
+	console.log("restr: " + restr)
+	console.log("fnify: " + fn)
+} else if (prog) {
+	console.log("Unrecognized program " + prog)
+	console.log("Valid values are parse, fnify, and debug\n")
+	process.exit()
+}
 
 let args = reduceargs(token)
 let argc = args.length
-let fn = fnify(token)
-console.log("fnify: " + fn)
-
 console.log(args.join(" ") + " " + expr)
 let perms = Math.pow(2, argc)
 for (let i = 0; i < perms; i++) {
