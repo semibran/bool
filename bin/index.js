8f88bc1260a506d22060173130d4596677652080
Brandon Semilla <semibran@gmail.com>
2020-10-28 20:58:31 -0700

    first pass on fnify

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,11 @@
 const parse = require("../lib/parse")
 const strify = require("../lib/strify")
+const fnify = require("../lib/fn")
 
 let expr = process.argv[2]
 let token = parse(expr)
 let restr = strify(token)
+let fn = fnify(expr)
 console.log("token: " + JSON.stringify(token))
 console.log("restr: " + restr)
+console.log("fn: " + fn)
